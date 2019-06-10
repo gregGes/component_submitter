498a09caad36bce4a1b3853246cf8c68475f603e
gregGes <gesmier.gregoire@gmail.com>
2019-06-10 18:46:00 +0200

    fix issue

--- adaptors/pk_adaptor.py
+++ adaptors/pk_adaptor.py
@@ -63,7 +63,6 @@ class PkAdaptor(abco.Adaptor):
                     self.pk_data[SCALING][SERVICES].append(service)
             logger.info("Policy of {0} is translated".format(target.name))
 
-        if self.validate is False:
         if tmp:
             self._yaml_write(self.tmp_path)
             logger.info("Updated PK file created")
